a10ce0bc60f362d4e189395e63d3ec7ec7af324d
Nick <189653818+outsidebagel@users.noreply.github.com>
2026-01-13 21:22:05 -0600

    Added comments & removed unused CSS class

--- src/main/java/solarcar/backend/services/DB.java
+++ src/main/java/solarcar/backend/services/DB.java
@@ -24,27 +24,26 @@ public class DB {
         this.query = db.getQueryApi();
     }
     
-    // Query the database for new data, only
+    // Query the database for new data
     public ArrayList<Object> getNewData() {
-        // stores our data to return
+        // Stores our data to return
         ArrayList<Object> returnList = new ArrayList<>();
 
-        // get our latest data from our db
-        // this query will return latest tables for each of our fields
-        // Change to telemetry_test
+        // Get our latest data from InfluxDB
+        // This query will return latest tables for each of our fields
         String flux = "from(bucket: \"Cardata\")\n" +
                 "  |> range(start: -1h)\n" +
                 "  |> filter(fn: (r) => r[\"_measurement\"] == \"dcBus\" or r[\"_measurement\"] == \"driveCMD\" or r[\"_measurement\"] == \"mainPackInfo\" or r[\"_measurement\"] == \"odoAndAmp\" or r[\"_measurement\"] == \"packVoltInfo\" or r[\"_measurement\"] == \"packTempInfo\" or r[\"_measurement\"] == \"velocity\")\n" +
                 "  |> filter(fn: (r) => r[\"_field\"] == \"amphours\" or r[\"_field\"] == \"avgCellV\" or r[\"_field\"] == \"dcCurrent\" or r[\"_field\"] == \"avgTemp\" or r[\"_field\"] == \"mainPackCurrent\" or r[\"_field\"] == \"dcVoltage\" or r[\"_field\"] == \"driveMotorRPM\" or r[\"_field\"] == \"hiCellID\" or r[\"_field\"] == \"lowCellID\" or r[\"_field\"] == \"velMotorRPM\" or r[\"_field\"] == \"vel\" or r[\"_field\"] == \"summedV\" or r[\"_field\"] == \"packAmp\" or r[\"_field\"] == \"odo\" or r[\"_field\"] == \"motorCurrent\" or r[\"_field\"] == \"lowTemp\" or r[\"_field\"] == \"lowCellV\" or r[\"_field\"] == \"instVolt\" or r[\"_field\"] == \"highTemp\" or r[\"_field\"] == \"hiCellV\")\n" +
                 "  |> last()";
 
-        // send the query
+        // Send the query
         List<FluxTable> tables = query.query(flux);
 
-        // loop through every table from the query
+        // Loop through every table from the query
         for (FluxTable table : tables){
             List<FluxRecord> records = table.getRecords();
-            // for each record (there is only one in each table in our current query) add
+            // For each record (there is only one in each table in our current query) add
             for (FluxRecord record : records){
                 returnList.add(new CarDataObject(record.getField(), (double) record.getValue()));
             }
@@ -52,7 +51,7 @@ public class DB {
         return returnList;
     }
 
-    // our key/value pair object to return our data
+    // Our key/value pair object to return our data
     private record CarDataObject(String field, double value) {}
 
 }
