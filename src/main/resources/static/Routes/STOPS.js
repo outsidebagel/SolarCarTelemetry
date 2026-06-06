24b8318cd34a8cbcaa76d2460c028b6341265491
solarsuncar123 <234073521+solarsuncar123@users.noreply.github.com>
2026-06-05 19:59:00 -0500

    Map with Road Race Route and car auto center when car is moving

---
+++ src/main/resources/static/Routes/STOPS.js
@@ -0,0 +1,257 @@
+export var STOPS = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.233891128706,
+          44.9760277327213,
+          0
+        ]
+      },
+      "properties": {
+        "name": "Start Line: Minneapolis, MN\n\n",
+        "description": "University of Minnesota",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.265937,
+          43.852118,
+          204.75
+        ]
+      },
+      "properties": {
+        "name": "Checkpoint: La Crosse, WI",
+        "description": "Summit Environmental Elementary",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -89.000652,
+          40.512925,
+          239.1
+        ]
+      },
+      "properties": {
+        "name": "Checkpoint: Normal, IL",
+        "description": "Illinois State University",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -90.001221,
+          38.793159,
+          166
+        ]
+      },
+      "properties": {
+        "name": "Stage Stop: Edwardsville, IL",
+        "description": "Southern Illinois University - Edwardsville",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.778229,
+          37.950448,
+          332.9
+        ]
+      },
+      "properties": {
+        "name": "Checkpoint: Rolla, MO",
+        "description": "Missouri S&T",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -94.314768,
+          37.175917,
+          300.1
+        ]
+      },
+      "properties": {
+        "name": "Stage Stop: Carthage, MO",
+        "description": "Memorial Hall & Boots Court Motel",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.016601,
+          36.107736,
+          202.8
+        ]
+      },
+      "properties": {
+        "name": "Checkpoint: Tulsa, OK",
+        "description": "Route 66 Historical Village",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -99.437241,
+          35.411766,
+          614
+        ]
+      },
+      "properties": {
+        "name": "Stage Stop: Elk City, OK",
+        "description": "National Route 66 & Transportation Museum",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -101.880622,
+          35.200518,
+          1122.9
+        ]
+      },
+      "properties": {
+        "name": "Finish Line: Amarillo, TX",
+        "description": "Arts in the Sunset",
+        "styleUrl": "#icon-1899-0288D1",
+        "label-scale": 0,
+        "icon-scale": 1,
+        "icon-offset": [
+          32,
+          64
+        ],
+        "icon-offset-units": [
+          "pixels",
+          "insetPixels"
+        ],
+        "icon": "images/icon-1.png",
+        "color": "#0b0bd4"
+      }
+    }
+  ]
+}
